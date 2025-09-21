03ac991acf364eeb7d049bc4c76f96f07cde0984
muthutm1701 <muthutm1701@gmail.com>
2025-09-21 19:50:47 +0530

    feat: calculate max depth of comment is created

---
+++ week1/Project-Social_Media_Analytics_Platform/src/commentMethods.js
@@ -0,0 +1,51 @@
+
+const { Comment } = require('../models');
+async function getCommentTree(postId) {
+    const comments = await Comment.findAll({
+        where: { postId: postId },
+        order: [['created_at', 'ASC']],
+        raw: true
+    });
+    const commentMap = new Map();
+    comments.forEach(comment => {
+        comment.replies = [];
+        commentMap.set(comment.id, comment);
+    });
+    const commentTree = [];
+    comments.forEach(comment => {
+        if (comment.parent_comment_id) {
+            const parent = commentMap.get(comment.parent_comment_id);
+            if (parent) {
+                parent.replies.push(comment);
+            }
+        } else {
+            commentTree.push(comment);
+        }
+    });
+
+    return commentTree;
+}
+function calculateMaxDepth(commentNode) {
+    if (!commentNode.replies || commentNode.replies.length === 0) {
+        return 1;
+    }
+    const childDepths = commentNode.replies.map(calculateMaxDepth);
+    return 1 + Math.max(...childDepths);
+}
+async function analyzeComments(postId) {
+    const commentTree = await getCommentTree(postId);
+
+    const analysis = commentTree.map(rootComment => {
+        const maxDepth = calculateMaxDepth(rootComment);
+        return {
+            commentId: rootComment.id,
+            content: rootComment.content,
+            engagementScore: {
+                maxDepth: maxDepth
+            }
+        };
+    });
+
+    return analysis;
+}
+module.exports = { analyzeComments };
